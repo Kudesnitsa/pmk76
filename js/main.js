39a8b66b8590a5aa13a4077b0fd7576eb1d55cf5
Kudesnitsa <diana199725@gmail.com>
2018-04-26 13:32:20 +0300

    add selector for page home

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 
 jQuery(document).ready(function () {
     jQuery('.icon_nav').on('click', function () {
-        console.log('sadasfas');
         jQuery('.menu_mob').toggleClass('mob');
+    });
+    jQuery('.select .sub_btn').click(function () {
+        var chosen = jQuery('.select .chosen');
+        chosen.html(jQuery(this).html());
+        jQuery(this).html(chosen.html());
     });
 
 
