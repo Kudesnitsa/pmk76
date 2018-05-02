4c9fca8e70cd482e5d13222c5ef4aec4b793a534
Kudesnitsa <diana199725@gmail.com>
2018-05-02 13:28:55 +0300

    edit page house

--- js/main.js
+++ js/main.js
@@ -3,19 +3,19 @@ jQuery(document).ready(function () {
     jQuery('.icon_nav').on('click', function () {
         jQuery('.menu_mob').toggleClass('mob');
     });
-    jQuery('.select .sub_btn').click(function () {
-        var chosen = jQuery('.select .chosen');
+    jQuery('.select.date .sub_btn').click(function () {
+        var chosen = jQuery('.select.date .chosen');
         chosen.html(jQuery(this).html());
         jQuery(this).html(chosen.html());
     });
 
 
 
-
-
-
-
-
+    jQuery('.select.infar .sub_btn').click(function () {
+        var chosen = jQuery('.select.infar .chosen');
+        chosen.html(jQuery(this).html());
+        jQuery(this).html(chosen.html());
+    });
 
 
 
